b952e4ae367f408d917daee831fd97c2c10735f0
Sabin Pokhrel <shubhampokhrel81@gmail.com>
2019-09-06 08:23:30 +0545

    Basic Layout

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import "package:flutter/material.dart";
+import 'package:note_app/screens/note_list.dart';
+
+
+void main(){
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'NoteApp',
+      theme: ThemeData(
+        primarySwatch: Colors.deepPurple
+      ),
+      home: NoteList(),
+    );
+  }
+
+}
