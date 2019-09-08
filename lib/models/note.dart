b3499151428abc7e061ce779b6efc219589a9a19
Sabin Pokhrel <shubhampokhrel81@gmail.com>
2019-09-09 00:06:44 +0545

    Map object created

--- lib/models/note.dart
+++ lib/models/note.dart
@@ -35,4 +35,20 @@ class Note{
   set date(String newDate){
     this._date= newDate;
   }
+  //Convert a Note object into a Map object
+
+  Map<String, dynamic> toMap(){
+    var map = Map<String, dynamic>();
+    if(id != null){
+      map['id'] = _id;
+    }
+
+    map['id'] = _id;
+    map['title']= _title;
+    map['description']= _description;
+    map['priority']= _priority;
+    map['date']= _date;
+
+    return map;
+  }
 }
